23c4b03e94854205a99b75cdc620680274ccef22
filbeofITK <filyo.bendeguz@hallgato.ppke.hu>
2022-02-25 18:55:34 +0100

    Webpage development. Created a Vue.js project, added Bootstrap, and navigation bar.
    Basic container of future cards also added.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar nav-pills navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link active">Home</router-link>
+          </li>
+          <li class="nav-item">
+              <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+        </ul>
+      </div>
+      <a class="navbar-brand" href="#">Preeclampsia adatbázis</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
   </nav>
   <router-view/>
+
+
 </template>
 
 <style>
